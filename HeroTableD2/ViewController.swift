1eef80e95566a600f54e69af6cbd1100456bf179
Sergey Kykhov <ghrle@mail.ru>
2023-05-18 12:19:50 +0300

    Initial Commit

---
+++ HeroTableD2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroTableD2
+//
+//  Created by Sergey Kykhov on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
